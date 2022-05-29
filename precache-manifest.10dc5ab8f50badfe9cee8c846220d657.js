0d6865b084e41a63430473c44feaf93ab0260554
simonsajji <simonmathai777@gmail.com>
2022-05-29 19:38:55 +0530

    Updates

---
+++ precache-manifest.10dc5ab8f50badfe9cee8c846220d657.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b2e296c922a567775a9b844e6f2036e",
+    "url": "/react_stopwatch/index.html"
+  },
+  {
+    "revision": "b835cbe2c749697fbb97",
+    "url": "/react_stopwatch/static/css/main.07f8f406.chunk.css"
+  },
+  {
+    "revision": "f1ceeae124cc2f12f13b",
+    "url": "/react_stopwatch/static/js/2.7cc8e56a.chunk.js"
+  },
+  {
+    "revision": "b835cbe2c749697fbb97",
+    "url": "/react_stopwatch/static/js/main.f4d7456b.chunk.js"
+  },
+  {
+    "revision": "acf39630d1c4bcaca32e",
+    "url": "/react_stopwatch/static/js/runtime~main.07023ce8.js"
+  }
+]);
